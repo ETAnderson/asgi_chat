9b6d4a6d195780b1bc9733ceccc9e931d5f2a6af
Eric Anderson <anderson.eric.t@gmail.com>
2021-07-29 20:45:00 -0400

    moved templates folder to the project level, added user signup

--- project/project/settings.py
+++ project/project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'channels',
     'chat',
     'django.contrib.admin',
--- project/project/urls.py
+++ project/project/urls.py
@@ -20,8 +20,9 @@ from django.contrib import admin
 urlpatterns = [
     # Chat
     path('chat/', include('chat.urls')),
-    path('admin/', admin.site.urls),
     # Auth
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
