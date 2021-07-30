480feb5c4992556c7d65f90e72f75cbb0d29d8c9
Eric Anderson <anderson.eric.t@gmail.com>
2021-07-29 20:11:02 -0400

    basic chat server functionality met

--- project/project/settings.py
+++ project/project/settings.py
@@ -129,3 +129,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Channels
 ASGI_APPLICATION = 'project.asgi.application'
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
