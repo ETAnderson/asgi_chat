9b2d3f8ba88c2eabc7c27cce425a4e9eb31a5205
Eric Anderson <anderson.eric.t@gmail.com>
2021-07-29 20:56:56 -0400

    filled out templates for password resets

--- project/chat/urls.py
+++ project/chat/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
     # Chat
     path('', views.index, name='index'),
     path('<str:room_name>/', views.room, name='room'),
-    # Auth
-    path('accounts/', include('django.contrib.auth.urls')),
 ]
--- project/project/settings.py
+++ project/project/settings.py
@@ -142,3 +142,6 @@ CHANNEL_LAYERS = {
 # Redirects
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
